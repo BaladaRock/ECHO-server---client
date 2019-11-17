7bfc478ad2704ccdea9760e0ee06b75dbf972f0b
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-11-18 00:27:13 +0200

    Echo Server - initial commit

---
+++ Echo_Server_Client/Echo_Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Echo_Server_Client
+{
+    internal class EchoServer
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Echo_Server_Client/Server/Server.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{
+    internal class Server
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
