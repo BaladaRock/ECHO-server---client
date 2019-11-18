d19daa2b9d476d95769f7478a5c56500fe0eb200
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-11-18 14:41:56 +0200

    Echo Server_Client - fully implemented

---
+++ Client/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Client
+    {
+        public Client(string serverIP, int dataPort)
+        {
+            TcpClient = new TcpClient(serverIP, dataPort);
+        }
+
+        public TcpClient TcpClient { get; set; }
+
+        internal static void Main(string[] args)
+        {
+            var client = new Client("localHost", 10000);
+
+            NetworkStream stream = client.TcpClient.GetStream();
+            stream.Write(Encoding.ASCII.GetBytes(Console.ReadLine()));
+
+
+            stream.Close();
+            client.TcpClient.Close();
+        }
+    }
+}
--- EchoServer_Client/EchoServer.cs
+++ EchoServer_Client/EchoServer.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace EchoServer
 {
     internal class EchoServer
     {
+        public EchoServer(IPAddress ip)
+        {
+            Server = new TcpListener(ip, 10000);
+            Client = default(TcpClient);
+        }
+
+        public TcpListener Server { get; set; }
+        public TcpClient Client { get; set; }
+
         internal static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const byte maxValue = byte.MaxValue;
+
+            var EchoServer = new EchoServer(Dns.GetHostEntry("localHost").AddressList[0]);
+            TcpListener server = EchoServer.Server;
+            TcpClient client = EchoServer.Client;
+            server.Start();
+
+            while (true)
+            {
+                byte[] buffer = new byte[maxValue];
+                NetworkStream dataStream = server.AcceptTcpClient().GetStream();
+
+                dataStream.Read(buffer, 0, maxValue);
+
+                Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, maxValue));
+                Console.ReadKey();
+            }
         }
     }
 }
